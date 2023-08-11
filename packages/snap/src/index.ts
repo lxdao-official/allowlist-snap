1868f2e055beb68a7b7fba84e19a8c54b6674ecb
CharlesHehe <hechenjuner@gmail.com>
2023-08-11 23:30:32 +1200

    first commit

---
+++ packages/snap/src/index.ts
@@ -0,0 +1,49 @@
+import { OnTransactionHandler } from '@metamask/snaps-types';
+import { heading, panel, text } from '@metamask/snaps-ui';
+
+interface Window {
+  ethereum: {
+    request: (args: { method: string;[key: string]: any }) => Promise<any>;
+    // You can add more Ethereum-specific properties and methods here if needed.
+  };
+} 
+
+// Handle outgoing transactions.
+export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
+
+  // Use the window.ethereum global provider to fetch the gas price.
+  const currentGasPrice = await window.ethereum.request({
+    method: 'eth_gasPrice',
+  });
+
+  // Get fields from the transaction object.
+  const transactionGas = parseInt(transaction.gas as string, 16);
+  const currentGasPriceInWei = parseInt(currentGasPrice  as string, 16);
+  const maxFeePerGasInWei = parseInt(transaction.maxFeePerGas as string, 16);
+  const maxPriorityFeePerGasInWei = parseInt(
+    transaction.maxPriorityFeePerGas as string,
+    16,
+  );
+
+  // Calculate gas fees the user would pay.
+  const gasFees = Math.min(
+    maxFeePerGasInWei * transactionGas,
+    (currentGasPriceInWei + maxPriorityFeePerGasInWei) * transactionGas,
+  );
+
+  // Calculate gas fees as percentage of transaction.
+  const transactionValueInWei = parseInt(transaction.value as string, 16);
+  const gasFeesPercentage = (gasFees / (gasFees + transactionValueInWei)) * 100;
+
+  // Display percentage of gas fees in the transaction insights UI.
+  return {
+    content: panel([
+      heading('Transaction insights snap'),
+      text(
+        `As set up, you are paying **${gasFeesPercentage.toFixed(
+          2,
+        )}%** in gas fees for this transaction.`,
+      ),
+    ]),
+  };
+};
