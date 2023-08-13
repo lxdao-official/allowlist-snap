5bb64ea16d0a8d3ef73d6ae27ecdc74537fb6ed6
CharlesHehe <hechenjuner@gmail.com>
2023-08-13 22:02:33 +1200

    correct some typos

--- packages/snap/src/check-allowlist.ts
+++ packages/snap/src/check-allowlist.ts
@@ -1,20 +1,26 @@
 import { OnTransactionHandler } from '@metamask/snaps-types';
 import Web3 from 'web3';
+
+
 // initiate an instance of Web3
 const web3 = new Web3('https://linea-goerli.infura.io/v3/e0f8e927116448549412b47e1f12bfe9');
+const smartContractAddress = '0x7b6D707583e25afF72DF7f13024F6E3D8C5216C9';
 
-
-
+/**
+ * Invoke allowlist verify RPC API in allowlist snap.
+ *
+ * @param addressToVerify - The "to" address of the transaction.
+ * @returns The result of allowlist verify RPC API.
+ */
 export async function checkResult(addressToVerify?: string) {
-  // encode functionSignature
+
   const functionSignature = "0x63a9c3d7";
-  // encode Parameters
+
   const encodedParams = web3.eth.abi.encodeParameters(['address'], [addressToVerify]);
 
   const encoded_data = functionSignature + encodedParams.slice(2);
 
   const url = 'https://linea-goerli.infura.io/v3/e0f8e927116448549412b47e1f12bfe9';
-  // https://linea-goerli.infura.io/v3/17cb760d66f944d383dd3e6d6129aa13
 
   const headers = {
     'Content-Type': 'application/json'
@@ -25,8 +31,8 @@ export async function checkResult(addressToVerify?: string) {
     method: 'eth_call',
     params: [
       {
-        from: '0x17c57bD297175e5711Ee3Daf045252B588f3162F',
-        to: '0x7b6D707583e25afF72DF7f13024F6E3D8C5216C9',
+        // from: '0x17c57bD297175e5711Ee3Daf045252B588f3162F',
+        to: smartContractAddress,
         gas: '0x10116',
         gasPrice: '0x9502F907',
         value: '',
@@ -45,27 +51,5 @@ export async function checkResult(addressToVerify?: string) {
 
   const response = await fetch(url, requestOptions);
   const responseData = await response.json();
-
-  // console.log(responseData.result);
-  console.log(responseData.result);
   return responseData.result;
 }
-
-
-
-// initiate an instance of Web3
-// const web3 = new Web3('https://linea-goerli.infura.io/v3/e0f8e927116448549412b47e1f12bfe9');
-
-// encode functionSignature
-// const functionSignature = web3.eth.abi.encodeFunctionSignature('checkResult(encoded_data?: string)');
-const functionSignature = "0x63a9c3d7";
-// encode Parameters
-const encodedParams = web3.eth.abi.encodeParameters(['address'], ['0x803Cfb2072d7c3728b8843548F5b86E7609c75b2']);
-
-const encoded_data = functionSignature + encodedParams.slice(2);
-console.log(functionSignature);
-console.log(encodedParams.slice(2));
-
-
-console.log(encoded_data);
-checkResult(encoded_data);
--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -5,19 +5,14 @@ import Web3 from 'web3';
 
 const web3 = new Web3('https://mainnet.infura.io/v3/17cb760d66f944d383dd3e6d6129aa13');
 
-
 function stringToBool(str: string): boolean {
-
   if (str[str.length - 1].includes("1")) return true;
-
   if (str[str.length - 1].includes("0")) return false;
-  console.log(str);
   throw new Error("Invalid string representation of boolean");
 }
 
 async function checkValue(addressToVerify: string) {
   const result = await checkResult(addressToVerify);
-
   const boolResult = stringToBool(result);
   return boolResult
 }
@@ -25,26 +20,22 @@ async function checkValue(addressToVerify: string) {
 // Handle outgoing transactions.
 export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
 
-
-
-  // const encoded_data = web3.eth.abi.encodeParameters(['address'], [transaction.to]);
-  // const result = await checkResult(encoded_data);
   const boolResult = await checkValue(transaction.to as string);
 
   if (boolResult) return {
     content: panel([
-      heading('allowlist snap'),
+      heading('allowlist tips'),
       text(
-        `The "to" address of this transaction is trustworthy.`,
-        // `**${result}%**`
+        `The "to" address (**${transaction.to as string}**) of this transaction is in the allowlist.`,
+        // `**${result}**`
       ),
     ]),
   };
   else return {
     content: panel([
-      heading('allowlist snap'),
+      heading('allowlist tips'),
       text(
-        `The "to" address of this transaction is untrustworthy`,
+        `The "to" address (**${transaction.to as string}**) of this transaction is NOT in the allowlist.`,
       ),
     ]),
   };
